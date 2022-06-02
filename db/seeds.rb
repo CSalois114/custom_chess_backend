387ea3dccd2dc5324a00e72fc7b830a85c45ac54
Chris Salois <csalois@gmail.com>
2022-06-01 20:06:55 -0400

    add enemies to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,38 @@ ditto = Piece.create(
   back_img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png",
 )
 
+squirtle = Piece.create(
+  game: test_game,
+  name: "squirtle",
+  front_img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+  back_img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/7.png"
+)
+
+squirtle_move_1 = Move.create(
+  piece: squirtle,
+  can_kill: true,
+  offset: "0,1"
+)
+
+squirtle_move_2 = Move.create(
+  piece: squirtle,
+  can_kill: true,
+  offset: "0,-1"
+)
+
+squirtle_move_3 = Move.create(
+  piece: squirtle,
+  can_kill: true,
+  offset: "1,0"
+)
+
+squirtle_move_4 = Move.create(
+  piece: squirtle,
+  can_kill: true,
+  offset: "-1,0"
+)
+
+
 m1 = Move.create(
   piece: ditto,
   can_kill: true,
@@ -36,11 +68,42 @@ m3 = Move.create(
 
 Deployment.create(
   piece: ditto,
+  starting_coords: "4,1",
+  coords: "4,1",
+  home_team: true,
+  image: ditto.back_img
+)
+
+Deployment.create(
+  piece: squirtle,
+  starting_coords: "5,1",
+  coords: "5,3",
+  home_team: true,
+  image: squirtle.back_img
+)
+
+Deployment.create(
+  piece: squirtle,
   starting_coords: "1,1",
   coords: "1,1",
   home_team: true,
-  image: ditto.front_img
+  image: squirtle.back_img
 )
 
+Deployment.create(
+  piece: squirtle,
+  starting_coords: "5,5",
+  coords: "5,5",
+  home_team: false,
+  image: squirtle.front_img
+)
+
+Deployment.create(
+  piece: squirtle,
+  starting_coords: "1,7",
+  coords: "1,7",
+  home_team: false,
+  image: squirtle.front_img
+)
 
 puts "✅ Done seeding!"
